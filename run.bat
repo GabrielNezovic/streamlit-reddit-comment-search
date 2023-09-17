4c4c7232549f7fb738114c3a2db4fa59bfab79f0
GabrielNezovic <gabriel.nezovic@gmail.com>
2023-09-18 09:15:20 +1000

    Initial Commit

---
+++ run.bat
@@ -0,0 +1,58 @@
+@echo off
+cd /d %~dp0
+echo The current directory is: %CD%
+
+rem Define the loading bar characters and length
+set "loading_chars=-"
+set "loading_length=60"
+
+rem Clear the error flag
+set "error_flag="
+
+rem Start the pip installation in the background
+start /b pip install -r requirements.txt >NUL 2>&1
+
+rem Enable delayed expansion
+setlocal enabledelayedexpansion
+
+rem Initialize loading bar
+set "loading_bar="
+
+rem Function to display loading bar
+:display_loading_bar
+cls
+echo Welcome to Reddit Comment Search!
+echo.&echo Checking on required packages:
+if not "!error_flag!"=="" (
+    echo.&echo Error: An error occurred during installation.
+    goto :end
+)
+
+rem Display the loading bar
+echo.&echo [!loading_bar!]
+
+rem Check if the pip installation process is still running
+tasklist /FI "IMAGENAME eq python.exe" | find /I "python.exe" >NUL
+if %ERRORLEVEL% EQU 0 (
+    set /a "loading_index+=1"
+    if !loading_index! gtr %loading_length% (
+        set "loading_index=0"
+        set "loading_bar="
+    ) else (
+        set "loading_bar=!loading_bar!!loading_chars!"
+    )
+    goto :display_loading_bar
+)
+
+rem If the pip installation process has finished, continue with the script
+goto :installation_complete
+
+:end
+rem Handle error and exit
+
+:installation_complete
+rem Continue with the rest of your script
+endlocal
+echo.&echo Required packages are available.
+echo.&echo Starting up...
+streamlit run search.py --server.port 1198 --browser.gatherUsageStats false --client.toolbarMode minimal --client.showErrorDetails false --runner.fastReruns true --theme.base dark --logger.level info
---
+++ search.py
@@ -0,0 +1,72 @@
+import praw
+import datetime
+import os
+import streamlit as st
+
+# HOW TO GET YOUR REDDIT API CLIENT_ID AND CLIENT_SECRET #
+
+# 1. Click on Create a new app in the reddit app console: https://www.reddit.com/prefs/apps
+# 2. Name the app 'Reddit Comment Search'
+# 3. Select 'Script' from the list of radio buttons
+# 4. Add ther description 'A python script to search through reddit comment history."
+# 5. Use your public IP address as the about url and the redirect uri
+# 6. Confirm your status as a human
+# 7. The client_id will be the short-ish string of random characters underneath the name of the application
+# 8. The client_secret will be the "secret" listed under the application when you click on edit application
+
+# Set up the Reddit API credentials
+reddit = praw.Reddit(
+    client_id='XXX',
+    client_secret='XXX',
+    user_agent='Reddit API Script'
+)
+
+def search_comments_by_user(username, query, date_range):
+    # Get the Reddit user
+    user = reddit.redditor(username)
+
+    # Convert date_range to an integer
+    date_range = int(date_range)
+
+    # Calculate the start and end dates based on the current date and days in the past
+    end_date = datetime.datetime.now()
+    start_date = end_date - datetime.timedelta(days=date_range)
+
+    # Convert the dates to Unix timestamps
+    start_timestamp = int(start_date.timestamp())
+    end_timestamp = int(end_date.timestamp())
+
+    # Get the comments by the user within the date range
+    comments = user.comments.new(limit=None)
+
+    # Create a folder named "comments" if it doesn't exist
+    if not os.path.exists("comments"):
+        os.makedirs("comments")
+
+    # Search for matching comments
+    for comment in comments:
+        if comment.created_utc >= start_timestamp and comment.created_utc <= end_timestamp:
+            if query.lower() in comment.body.lower():
+                # Generate the filename based on username and timestamp
+                filename = f" {int(comment.created_utc)} - {username} - {query}.txt"
+                # Save the comment content to a text file in the "comments" folder
+                with open(os.path.join("comments", filename), "w", encoding="utf-8") as file:
+                    file.write(comment.body)
+                    st.success(filename)
+
+st.title("Reddit Username Search")
+# Retrieve and save matching comments
+with st.form("Search"):
+    col1,col2,col3 = st.columns(3)
+    with col1:
+        username = st.text_input("Username", placeholder="Snoo")
+    with col2:
+        query = st.text_input("Search Query", placeholder="Alien")
+    with col3:
+        date_range = st.text_input("Date Filter", placeholder="365", help="How many days of comment history to filter (from todays date), eg to filter the last 1 year of comments, type '365'")
+    
+    if st.form_submit_button("Search Comments", type="secondary", use_container_width=True):
+        with st.status("Search Results"):
+            search_comments_by_user(username, query, date_range)
+    
+        st.success("Matching comments saved to the 'comments' folder.")
